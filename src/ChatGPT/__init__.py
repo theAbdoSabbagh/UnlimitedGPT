2b3a7d17341b5eb1e0804d950cd84eeed400d9dd
Sxvxgee <aboud1xy@gmail.com>
2023-05-27 14:22:39 +0400

    init

---
+++ src/ChatGPT/__init__.py
@@ -0,0 +1,10 @@
+"""
+ChatGPT.py
+
+An unofficial Python wrapper for OpenAI's ChatGPT API
+"""
+
+from .ChatGPT import ChatGPT
+from .internal.objects import ChatGPTResponse
+from .internal.chatgpt_data import ChatGPTVariables
+from .internal.driver import ChatGPTDriver
