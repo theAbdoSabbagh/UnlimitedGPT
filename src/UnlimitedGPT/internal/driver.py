1d2f2384ea7b10a5f483b1bc8bf8f4fedf30a07f
Sxvxgee <aboud1xy@gmail.com>
2023-05-28 18:38:20 +0400

    new custom driver function

--- src/UnlimitedGPT/internal/driver.py
+++ src/UnlimitedGPT/internal/driver.py
@@ -1,4 +1,6 @@
 import undetected_chromedriver as uc
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class ChatGPTDriver(uc.Chrome):
     """
@@ -7,3 +9,25 @@ class ChatGPTDriver(uc.Chrome):
     """
     def __init__(self, options: uc.ChromeOptions, headless: bool = False):
         super().__init__(options=options, headless=headless)
+
+    def safe_click(self, mark, timeout: int = 10) -> bool:
+        """
+        Clicks an element, and if it fails, tries again.
+
+        Args:
+        ----------
+            mark: (By, str): The element to click.
+            timeout: (int): The amount of time to wait for the element to be clickable.
+        
+        Returns:
+        ----------
+            bool: Whether or not the element was clicked.
+        """
+        wait = WebDriverWait(self, timeout)
+        try:
+            element = wait.until(EC.element_to_be_clickable(mark))
+            element.click()
+        except:
+            return False
+        else:
+            return True
