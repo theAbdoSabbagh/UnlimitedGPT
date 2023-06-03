02a9ea043efddc9b4f694bdce4d88aee4cecbc97
Sxvxgee <aboud1xy@gmail.com>
2023-06-04 01:40:07 +0400

    renamed variables, updated values

--- src/UnlimitedGPT/internal/chatgpt_data.py
+++ src/UnlimitedGPT/internal/chatgpt_data.py
@@ -3,77 +3,84 @@ from selenium.webdriver.common.by import By
 
 @dataclass
 class ChatGPTVariables:
+    # Other
+    outer_html = (By.XPATH, "/html")
     cf_challenge_form = (By.ID, 'challenge-form')
-
-    chatgpt_outer_html = (
-        By.XPATH,
-        "/html"
-    )
-    chatgpt_textbox = (
-        By.XPATH,
-        '/html/body/div[1]/div[2]/div[2]/div/main/div[3]/form/div/div/textarea'
-    )
-    chatgpt_info_buttons = [
-        (By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div/div[2]/button'),
-        (By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div/div[2]/button[2]'),
-        (By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div/div[2]/button[2]'),
-    ]
-    chatgpt_streaming = (
-        By.XPATH,
-        '//div[starts-with(@class, "result-streaming markdown prose")]'
-    )
-    chatgpt_big_response = (
-        By.XPATH,
-        '//div[@class="flex-1 overflow-hidden"]//div[p]'
-    )
-    chatgpt_small_response = (
-        By.XPATH,
-        '//div[starts-with(@class, "markdown prose w-full break-words")]',
-    )
-    chatgpt_alert = (
-        By.XPATH,
-        '//div[@role="alert"]'
-    )
-    chatgpt_intro = (
-        By.ID,
-        'headlessui-portal-root'
-    )
-
-    chatgpt_new_chat = (
-        By.LINK_TEXT,
-        'New chat'
-    )
-    chatgpt_chats_list_first_node = (
+    chats_list_first_node = (
         By.XPATH,
         '//div[substring(@class, string-length(@class) - string-length("text-sm") + 1)  = "text-sm"]//a',
     )
-    chatgpt_menu_button = (
-        By.XPATH,
-        "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[3]/div/button"
+
+    # Popups and such
+    info_buttons = (
+        (By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div/div[2]/button'),
+        (By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div/div[2]/button[2]'),
+        (By.XPATH, '/html/body/div[3]/div/div/div/div[2]/div/div[2]/button[2]'),
     )
-    chatgpt_menu_clear_conversations = (
+    alert = (
+        By.XPATH,
+        '//div[@role="alert"]'
+    )
+    intro = (
+        By.ID,
+        'headlessui-portal-root'
+    )
+    
+    # Responses and such
+    streaming = (
+        By.XPATH,
+        '//div[starts-with(@class, "result-streaming markdown prose")]'
+    )
+    big_response = (
+        By.XPATH,
+        '//div[@class="flex-1 overflow-hidden"]//div[p]'
+    )
+    small_response = (
+        By.XPATH,
+        '//div[starts-with(@class, "markdown prose w-full break-words")]',
+    )
+    textbox = (
+        By.XPATH,
+        '/html/body/div[1]/div[2]/div[2]/div/main/div[2]/form/div/div/textarea'
+    )
+    regenerate_response = (
+        By.XPATH,
+        "/html/body/div[1]/div[2]/div[2]/div/main/div[2]/form/div/div[1]/div/button"
+    )
+    new_chat = (
+        By.LINK_TEXT,
+        'New chat'
+    )
+
+    # Menu buttons
+    menu_button = (
+        By.XPATH,
+        "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[4]/div/button"
+    )
+    menu_clear_conversations = (
         # By.XPATH,
         # "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[3]/div/div/nav/a[2]"
         By.LINK_TEXT,
         'Clear conversations'
-    ) # Confirm button is the same path
-    chatgpt_menu_settings_button = (
+    )
+    menu_confirm_clear_conversations = (
+        By.LINK_TEXT,
+        "Confirm clear conversations"
+    )
+    menu_settings = (
         # By.XPATH,
         # "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[3]/div/div/nav/a[3]"
         By.LINK_TEXT,
         'Settings'
     )
-    chatgpt_regenerate_response_button = (
-        By.XPATH,
-        "/html/body/div[1]/div[2]/div[2]/div/main/div[3]/form/div/div[1]/div/button"
-    )
-    chatgpt_theme_select = (
+    theme_select = (
         By.CSS_SELECTOR,
         'select.rounded'
     )
-    chatgpt_data_controls_button = (
+    data_controls = (
         By.CSS_SELECTOR,
         'button[data-state="inactive"][id^="radix-"][id$="-trigger-DataControls"]'
     )
-
-    chatgpt_chat_url = 'https://chat.openai.com/chat'
+    
+    # URLs
+    chat_url = 'https://chat.openai.com/chat'
