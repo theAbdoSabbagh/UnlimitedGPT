d877bb89fe00e5bb27981ea4e071929d807e3eb4
Sxvxgee <aboud1xy@gmail.com>
2023-06-13 02:24:20 +0400

    updated imports

--- src/UnlimitedGPT/__init__.py
+++ src/UnlimitedGPT/__init__.py
@@ -6,5 +6,6 @@ An unofficial Python wrapper for OpenAI's ChatGPT API
 
 from .internal.chatgpt_data import ChatGPTVariables
 from .internal.driver import ChatGPTDriver
-from .internal.objects import ChatGPTResponse, SessionData, User
+from .internal.objects import *
+from .internal.exceptions import *
 from .UnlimitedGPT import ChatGPT
