70003214835c2c9a367fb1b47d6fc8ac2aed6f0b
Sxvxgee <aboud1xy@gmail.com>
2023-06-26 02:16:42 +0400

    updated selectors data

--- src/UnlimitedGPT/internal/chatgpt_data.py
+++ src/UnlimitedGPT/internal/chatgpt_data.py
@@ -32,14 +32,14 @@ class ChatGPTVariables:
         By.XPATH,
         '//div[starts-with(@class, "markdown prose w-full break-words")]',
     )
-    textbox = (By.XPATH, '//textarea[@id="prompt-textarea"]')
+    textbox = (By.XPATH, '//*[@id="prompt-textarea"]')
     continue_regenerating = (
         By.XPATH,
-        "/html/body/div[1]/div[2]/div[2]/div/main/div[3]/form/div/div[1]/div/button[2]",
+        "/html/body/div[1]/div[1]/div[2]/div/main/div[3]/form/div/div[1]/div/button[2]",
     )
     regenerate_response = (
         By.XPATH,
-        "/html/body/div[1]/div[2]/div[2]/div/main/div[3]/form/div/div[1]/div/button",
+        "/html/body/div[1]/div[1]/div[2]/div/main/div[3]/form/div/div[1]/div/button",
     )
     new_chat = (By.LINK_TEXT, "New chat")
     clear_chat = (By.LINK_TEXT, "Clear chat")
@@ -47,29 +47,24 @@ class ChatGPTVariables:
     # Menu buttons
     menu_button = (
         By.XPATH,
-        "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[4]/div/button",
+        "/html/body/div[1]/div[1]/div[1]/div/div/div/nav/div[4]/div/button",
     )
     menu_clear_conversations = (
-        # By.XPATH,
-        # "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[3]/div/div/nav/a[2]"
         By.LINK_TEXT,
         "Clear conversations",
     )
     menu_confirm_clear_conversations = (By.LINK_TEXT, "Confirm clear conversations")
     menu_settings = (
-        # By.XPATH,
-        # "/html/body/div[1]/div[2]/div[1]/div/div/div/nav/div[3]/div/div/nav/a[3]"
         By.LINK_TEXT,
         "Settings",
     )
-    theme_select = (By.CSS_SELECTOR, "select.rounded")
+    theme_button = (By.CSS_SELECTOR, "button[role='combobox']")
     data_controls = (
         By.CSS_SELECTOR,
         'button[data-state="inactive"][id^="radix-"][id$="-trigger-DataControls"]',
     )
     shared_links_manage = (
         By.XPATH,
-        # "/html/body/div[5]/div/div/div/div[2]/div[1]/div[4]/div/div[2]/div/button"
         "//button[.//div[text()='Manage']]"
     )
 
